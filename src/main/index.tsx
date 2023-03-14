3f93bc97bf7680f505c8a0abe37bd238ab6b6218
lucazweb <lucazwebmail@gmail.com>
2023-03-13 21:51:00 -0300

    chore: setup eslint

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement)
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 
-const App = () => <pre>Start Component</pre>
+const App = () => <pre>Start Component</pre>;
 
 root.render(<App />);
