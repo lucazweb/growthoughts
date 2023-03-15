f0eef3d67d9f1cd0fd7f730f9fb8be9d14748c64
Lucas <lucazwebmail@gmail.com>
2023-03-15 12:20:02 -0300

    refactor: add new tailwind classes to card component

--- src/presentation/components/card/card.tsx
+++ src/presentation/components/card/card.tsx
@@ -4,7 +4,7 @@ export const Card = ({ children }: PropsWithChildren) => {
   return (
     <div
       data-testid="card-component"
-      className="box-content rounded-lg h-96 w-96 p-4 border-1 shadow-lg  hover:shadow-md"
+      className="box-content rounded-lg border border-gray-200 mt-3 mb-3 h-96 w-96 p-4 border-1 shadow-lg  hover:shadow-md mx-auto"
     >
       {children}
     </div>
