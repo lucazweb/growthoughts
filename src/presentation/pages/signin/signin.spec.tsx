fb6dd37361f8d40ed7b92b70076860b9ee8e199d
Lucas <lucazwebmail@gmail.com>
2023-03-15 12:21:56 -0300

    test: ensure signin page basic elements render correctlly

---
+++ src/presentation/pages/signin/signin.spec.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import { SignIn } from './signin.page'
+
+const makeSut = () => {
+  const page = render(<SignIn />)
+  const { getByTestId } = page
+  return {
+    page,
+    getByTestId,
+  }
+}
+
+describe('signin page tests', () => {
+  test('should render sign-in page correctlly ', () => {
+    const page = makeSut()
+    const { getByTestId } = page
+
+    const form = getByTestId('signin-form')
+    expect(form).toBeTruthy()
+  })
+
+  test('should start with button sigin-in disabled', () => {
+    const page = makeSut()
+    const { getByTestId } = page
+    const button = getByTestId('signin-button')
+    expect(button).toBeDisabled()
+  })
+
+  test('should render signup button', () => {
+    const page = makeSut()
+    const { getByTestId } = page
+    const button = getByTestId('signup-button')
+    expect(button).toBeTruthy()
+  })
+
+  // test('should display error in case of invalid e-mail', () => {
+  //   expect(true).toBe(false)
+  // })
+
+  // test('should allow button click if email and password fields are valid', () => {
+  //   const page = makeSut()
+  //   const { getByTestId } = page
+  //   const button = getByTestId('signin-button')
+  //   expect(button).not.toHaveProperty('disabled')
+  // })
+
+  // test('should display error if credentials are invalid ', () => {
+  //   expect(true).toBe(false)
+  // })
+
+  // test('should redirect user if credentials are valid ', () => {
+  //   expect(true).toBe(false)
+  // })
+})
