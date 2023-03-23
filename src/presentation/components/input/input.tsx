d96ac98f8b164bb845465b8132559a8ad086861e
Lucas <lucazwebmail@gmail.com>
2023-03-22 22:50:48 -0300

    refactor: remove isInvalid prop of Input component

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -1,17 +1,11 @@
 import React, { type InputHTMLAttributes, useState } from 'react'
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  isInvalid?: boolean
   errorMessage?: string
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-export const Input = ({
-  isInvalid,
-  errorMessage,
-  onChange,
-  ...rest
-}: InputProps) => {
+export const Input = ({ errorMessage, onChange, ...rest }: InputProps) => {
   const [value, setValue] = useState<string>('')
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -25,12 +19,12 @@ export const Input = ({
         {...rest}
         data-testid="input-component"
         className={`w-full rounded-md border ${
-          isInvalid ? 'border-red-700' : 'border-[#e0e0e0]'
+          errorMessage ? 'border-red-700' : 'border-[#e0e0e0]'
         } bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md mt-3`}
         onChange={handleChange}
         value={value}
       />
-      {isInvalid && (
+      {errorMessage && (
         <p
           data-testid="input-error-message"
           className="text-red-700 mt-1 text-sm"
