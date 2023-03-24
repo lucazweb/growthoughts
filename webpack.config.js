a9ea0ef8499a60652f356bd1f638f61f4e5760d3
Lucas <lucazwebmail@gmail.com>
2023-03-24 16:57:49 -0300

    fix: router

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,64 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  mode: "development",
+  mode: 'development',
   entry: './src/main/index.tsx',
   output: {
     path: path.resolve(__dirname, 'public/js'),
     publicPath: 'public/js',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   module: {
-    rules: [{
-      test: /\.ts(x?)$/,
-      loader: 'ts-loader',
-      exclude: /\node_modules/,
-    },{
-      test: /\.css$/i,
-      include: path.resolve(__dirname, 'src'),
-      use: ['style-loader', 'css-loader', 'postcss-loader'],
-    },
-    {
-      test: /\.scss$/,
-      use: [
-        {
-          loader: 'style-loader',
-        },
-        {
-          loader: 'css-loader',
-          options: {
-            module: true,
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        loader: 'ts-loader',
+        exclude: /\node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, 'src'),
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader',
           },
-        },
-        {
-          loader: 'sass-loader',
-        },
-      ],
-    },]
+          {
+            loader: 'css-loader',
+            options: {
+              module: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+    ],
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.scss'],
     alias: {
-      '@': path.join(__dirname, 'src')
-    }
+      '@': path.join(__dirname, 'src'),
+    },
   },
   devServer: {
     static: {
       directory: './public',
     },
     devMiddleware: {
-      writeToDisk: true
+      writeToDisk: true,
     },
     liveReload: true,
+    historyApiFallback: true,
   },
-  plugins: [new HtmlWebpackPlugin({
-    template: './public/index.html'
-  })]
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
 }
