6cbcdfe5907b24ecbf7c2db8c74c65f1445141db
Lucas <lucazwebmail@gmail.com>
2023-03-28 18:56:19 -0300

    refactor: add card-progress props and styles

--- src/presentation/components/card-progress/card-progress.tsx
+++ src/presentation/components/card-progress/card-progress.tsx
@@ -1,17 +1,35 @@
 import React from 'react'
-import { CircularProgressbar } from 'react-circular-progressbar'
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar'
 
-const percentage = 66
-export const CardProgress = () => {
+type Props = {
+  title: string
+  status: string
+  endDate: string
+  percentage: number
+}
+
+export const CardProgress = ({ title, percentage, status, endDate }: Props) => {
   return (
-    <div className="flex items-start rounded-xl bg-white p-4 border border-gray-300 shadow-lg">
+    <div className="flex items-start rounded-xl bg-white p-4 border border-gray-300 shadow-lg max-w-md">
       <div className="flex h-20 w-20 items-center justify-center rounded-full border border-blue-100 bg-blue-50">
-        <CircularProgressbar value={percentage} text={`${percentage}%`} />
+        <CircularProgressbar
+          value={percentage}
+          text={`${percentage}%`}
+          background
+          backgroundPadding={6}
+          styles={buildStyles({
+            backgroundColor: '#3e98c7',
+            textColor: '#fff',
+            pathColor: '#fff',
+            trailColor: 'transparent',
+          })}
+        />
       </div>
 
       <div className="ml-4">
-        <h2 className="font-semibold">3 meses de academia</h2>
-        <p className="mt-2 text-sm text-gray-500">Last opened 4 days ago</p>
+        <h2 className="font-semibold">{title}</h2>
+        <p className="mt-2 text-sm text-gray-500">{status}</p>
+        <p className="mt-2 text-sm text-gray-500"> Termina em {endDate}</p>
       </div>
     </div>
   )
