3ff9cbe75c436bc5f33b67f306064d203b0fc4c5
Lucas <lucazwebmail@gmail.com>
2023-03-31 10:41:04 -0300

    feat: ensure AxiosHttpClient return correct response and status code

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,8 +1,19 @@
 import { type HttpPostParams } from '@/data/protocols/http-post-client'
-import axios from 'axios'
+import axios, { type AxiosResponse } from 'axios'
 
 export class AxiosHttpClient {
-  async post(params: HttpPostParams<any>): Promise<void> {
-    await axios(params.url)
+  async post(params: HttpPostParams<any>): Promise<any> {
+    let httpResponse: AxiosResponse<any>
+
+    try {
+      httpResponse = await axios.post(params.url, params.body)
+    } catch (error) {
+      httpResponse = error.response
+    }
+
+    return {
+      data: httpResponse,
+      statusCode: httpResponse.status,
+    }
   }
 }
