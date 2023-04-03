c790a822508e6434a971b1ac6235e2409487c017
Lucas <lucazwebmail@gmail.com>
2023-04-02 21:40:55 -0300

    chore: add support to svg imports

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,17 @@ module.exports = {
         use: ['@svgr/webpack'],
       },
       {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'svg-url-loader',
+            options: {
+              limit: 10000,
+            },
+          },
+        ],
+      },
+      {
         test: /\.scss$/,
         use: [
           {
